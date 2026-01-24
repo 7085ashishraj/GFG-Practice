95415c8ce8c30aff26c75aac6b1a1735da2e2905
7085ashishraj <145831668+7085ashishraj@users.noreply.github.com>
2026-01-24 13:22:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
@@ -7,16 +7,16 @@ class Solution {
         int n = arr.size();
         
         int i = 0;
-        int j = n-1;
-        
-        while(i <= j){
-            if(arr[i]==0) i++;
-            else if(arr[j]==1) j--;
+        int j = n - 1;
+        while(i<=j){
+            if(arr[i] == 0) i++;
+            else if(arr[j] == 1) j--;
             else{
                 swap(arr[i],arr[j]);
                 i++;
                 j--;
             }
+            
         }
         
         
